5368b28fb9f7d9d4906c3e67530335d511c39e34
joao.rosa <joao.rosa@idwall.co>
2022-06-28 23:55:06 -0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import dotenv from "dotenv";
-import "reflect-metadata";
-import express from "express";
-import "./shared/container";
-import "./database";
-import { router } from "./routes";
-import swaggerUI from "swagger-ui-express";
-import swaggerFile from "./swagger.json";
-import cors from "cors";
+import dotenv from 'dotenv';
+import 'reflect-metadata';
+import express from 'express';
+import './shared/container';
+import './database';
+import { router } from './routes';
+import cors from 'cors';
 
 const app = express();
 
@@ -15,10 +13,8 @@ app.use(cors());
 
 app.use(router);
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
-
 dotenv.config();
 
 app.listen(process.env.PORT || 3333, () =>
-  console.log("Ther server is running")
+  console.log('Ther server is running'),
 );
